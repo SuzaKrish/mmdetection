34aeb9bc0e1d362200ff30de5090ddbdd113eaaf
sean <linxuxin6@gmail.com>
2020-01-05 01:13:37 +0800

    rewrite _filter_imgs in wider_face.py

--- mmdet/datasets/wider_face.py
+++ mmdet/datasets/wider_face.py
@@ -6,6 +6,7 @@ import mmcv
 from .registry import DATASETS
 from .xml_style import XMLDataset
 
+import numpy as np
 
 @DATASETS.register_module
 class WIDERFaceDataset(XMLDataset):
@@ -17,6 +18,7 @@ class WIDERFaceDataset(XMLDataset):
     CLASSES = ('face', )
 
     def __init__(self, **kwargs):
+        self.box_min_size = kwargs['min_size'] if 'min_size' in kwargs.keys() else None
         super(WIDERFaceDataset, self).__init__(**kwargs)
 
     def load_annotations(self, ann_file):
@@ -40,3 +42,44 @@ class WIDERFaceDataset(XMLDataset):
                     height=height))
 
         return img_infos
+
+    def _filter_imgs(self, min_size=32):
+        """Filter images too small or without required ground truths."""
+        valid_inds = []
+        for i, img_info in enumerate(self.img_infos):
+            if len(self.get_boxes_info(i)) == 0:
+                continue
+            if min(img_info['width'], img_info['height']) >= min_size:
+                valid_inds.append(i)
+        return valid_inds
+
+    def get_boxes_info(self, idx):
+        img_id = self.img_infos[idx]['id']
+        xml_path = osp.join(self.img_prefix, 'Annotations',
+                            '{}.xml'.format(img_id))
+        tree = ET.parse(xml_path)
+        root = tree.getroot()
+        bboxes = []
+        for obj in root.findall('object'):
+            difficult = int(obj.find('difficult').text)
+            bnd_box = obj.find('bndbox')
+            bbox = [
+                int(bnd_box.find('xmin').text),
+                int(bnd_box.find('ymin').text),
+                int(bnd_box.find('xmax').text),
+                int(bnd_box.find('ymax').text)
+            ]
+            ignore = False
+            if self.box_min_size:
+                assert not self.test_mode
+                w = bbox[2] - bbox[0]
+                h = bbox[3] - bbox[1]
+                if w < self.box_min_size or h < self.box_min_size:
+                    ignore = True
+            if not difficult and not ignore:
+                bboxes.append(bbox)
+        if not bboxes:
+            bboxes = np.zeros((0, 4))
+        else:
+            bboxes = np.array(bboxes, ndmin=2) - 1
+        return bboxes.astype(np.float32)
