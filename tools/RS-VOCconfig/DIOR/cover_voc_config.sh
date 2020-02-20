982a725ca3ef94f9c2245d0007144d57295ee2fb
NicholasIrving <NicholasIrving@outlook.com>
2020-02-20 14:01:22 +0800

    Update cover_voc_config.sh

--- tools/RS-VOCconfig/DIOR/cover_voc_config.sh
+++ tools/RS-VOCconfig/DIOR/cover_voc_config.sh
@@ -1,2 +1,4 @@
 cp voc.py ../../../mmdet/datasets/
 cp xml_style.py ../../../mmdet/datasets/
+cp {eval_hooks.py,mean_ap.py,class_names.py} ../../../mmdet/core/evaluation/
+cp voc_eval.py ../../../tools/
