51ba75ade4a34c150ba60b37c3c3eb2f0ad0f212
NicholasIrving <NicholasIrving@outlook.com>
2020-02-29 14:13:25 +0800

    Add files via upload

---
+++ tools/RS-VOCconfig/DOTA/cover_voc_config.sh
@@ -0,0 +1,6 @@
+cp voc.py ../../../mmdet/datasets/
+cp xml_style.py ../../../mmdet/datasets/
+cp eval_hooks.py ../../../mmdet/core/evaluation/
+cp mean_ap.py ../../../mmdet/core/evaluation/
+cp class_names.py ../../../mmdet/core/evaluation/
+cp voc_eval.py ../../../tools/
