d5ec9ce598dc9f55266c41b5374acf66272ceedc
sean <linxuxin6@gmail.com>
2020-03-12 23:49:43 +0800

    update custom_test.py and moon_crater.py

--- mmdet/datasets/moon_crater.py
+++ mmdet/datasets/moon_crater.py
@@ -1,12 +1,33 @@
+import os
 import os.path as osp
+import shutil
 import xml.etree.ElementTree as ET
 
 import mmcv
+from mmdet.core import eval_map, eval_recalls
 
 from .registry import DATASETS
 from .xml_style import XMLDataset
 
 import numpy as np
+
+
+def check_fileDir(filePath, cover=0):
+    out_file_dir = os.path.split(filePath)[0]
+    if not os.path.isdir(out_file_dir):
+        os.makedirs(out_file_dir)
+    elif cover:
+        shutil.rmtree(out_file_dir)
+        os.makedirs(out_file_dir)
+
+
+def get_valFilePath(saveDir, imgPath):
+    imgPathList = imgPath.split('/')
+    if imgPath.find('DeepMoon'):
+        return os.path.join(saveDir, '/'.join(imgPathList[-3:-1]), imgPathList[-1].split('.')[0] + '.txt')
+    else:
+        return os.path.join(saveDir, imgPathList[-1].split('.')[0] + '.txt')
+
 
 @DATASETS.register_module
 class MOONCraterDataset(XMLDataset):
@@ -84,7 +105,6 @@ class MOONCraterDataset(XMLDataset):
             labels_ignore=labels_ignore.astype(np.int64))
         return ann
 
-
     def get_boxes_info(self, idx):
         img_id = self.img_infos[idx]['id']
         xml_path = osp.join(self.img_prefix, 'Annotations',
@@ -118,3 +138,43 @@ class MOONCraterDataset(XMLDataset):
         else:
             bboxes = np.array(bboxes, ndmin=2) - 1
         return bboxes.astype(np.float32)
+
+    def format_results(self, results, **kwargs):
+        data_loader = kwargs['data_loader']
+        save_dir = kwargs['save_dir']
+        prog_bar = mmcv.ProgressBar(len(data_loader))
+        for i, data in enumerate(data_loader):
+            img_path = data['img_meta'][0].data[0][0]['filename']
+            im_save_path = get_valFilePath(save_dir, img_path)
+            check_fileDir(im_save_path)
+            result_file = open(im_save_path, 'w')
+            result_file.write('{}\n'.format(img_path))
+            dets = results[i][0]
+            for det in dets:
+                bbox = det[:4]
+                score = det[4]
+                result_file.write('{:.1f} {:.1f} {:.1f} {:.1f} {:.3f}\n'.
+                                 format(bbox[0], bbox[1],
+                                        bbox[2], bbox[3],
+                                        score))
+            result_file.close()
+            prog_bar.update()
+
+    def evaluate(self, results, metric='voc', iou_thr=0.5, nproc=4, **kwargs):
+        if 'logger' in kwargs.keys():
+            logger = kwargs['logger']
+        else:
+            logger = 'print'
+        annotations = [self.get_ann_info(i) for i in range(len(self))]
+        dataset_name = self.CLASSES
+        if metric[0] != 'voc':
+            print('only support the voc evaluation style')
+        else:
+            eval_map(
+                results,
+                annotations,
+                scale_ranges=None,
+                iou_thr=iou_thr,
+                dataset=dataset_name,
+                logger=logger,
+                nproc=nproc)
