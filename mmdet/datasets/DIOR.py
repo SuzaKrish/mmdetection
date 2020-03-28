e53e4049429c558f78cbca3174fe59309bb1255c
NicholasIrving <NicholasIrving@outlook.com>
2020-03-28 13:48:54 +0800

    Add files via upload

--- mmdet/datasets/DIOR.py
+++ mmdet/datasets/DIOR.py
@@ -17,9 +17,3 @@ class DIORDataset(XMLDataset):
 
     def __init__(self, **kwargs):
         super(DIORDataset, self).__init__(**kwargs)
-        if 'VOC2007' in self.img_prefix:
-            self.year = 2007
-        elif 'VOC2012' in self.img_prefix:
-            self.year = 2012
-        else:
-            raise ValueError('Cannot infer dataset year from img_prefix')
